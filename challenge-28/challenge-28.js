dc3c5499ccf8dcac90a70f7f56de69622087418a
Lucas Morais <lucas.moraissz07@gmail.com>
2018-01-22 17:04:26 -0200

    Challenge-28

--- challenge-28/challenge-28.js
+++ challenge-28/challenge-28.js
@@ -1,3 +1,5 @@
+(function () {
+  'use strict';
   /*
   No HTML:
   - Crie um formulário com um input de texto que receberá um CEP e um botão
@@ -25,3 +27,142 @@
   - Utilize a lib DOM criada anteriormente para facilitar a manipulação e
   adicionar as informações em tela.
   */
+
+  function DOM(selectedNodes) {
+    this.element = document.querySelectorAll(selectedNodes);
+  }
+
+  DOM.prototype.on = function on(event, callback) {
+    Array.prototype.forEach.call(this.element, function (value) {
+      value.addEventListener(event, callback, false);
+    });
+  }
+
+  DOM.prototype.off = function off(event, callback) {
+    Array.prototype.forEach.call(this.element, function (value) {
+      value.removeEventListener(event, callback, false);
+    });
+  }
+  DOM.prototype.forEach = function (callback) {
+    Array.prototype.forEach.call(this.element, callback);
+  }
+
+  DOM.prototype.map = function (callback) {
+    Array.prototype.map.call(this.element, callback);
+  }
+
+  DOM.prototype.filter = function (callback) {
+    Array.prototype.filter.call(this.element, callback);
+  }
+
+  DOM.prototype.reduce = function (callback) {
+    Array.prototype.reduce.call(this.element, callback);
+  }
+
+  DOM.prototype.reduceRight = function (callback) {
+    Array.prototype.reduceRight.call(this.element, callback);
+  }
+
+  DOM.prototype.every = function (callback) {
+    Array.prototype.every.call(this.element, callback);
+  }
+
+  DOM.prototype.some = function (callback) {
+    Array.prototype.some.call(this.element, callback);
+  }
+
+  DOM.prototype.get = function get() {
+    return this.element;
+  }
+
+
+  var ajax = new XMLHttpRequest;
+  var $formCEP = new DOM('[data-js="formCEP"]');
+  var $inputMessage = document.querySelector('[data-js="textAreaForMessage"]');
+  var $inputsToFill = new DOM('[data-js="inputToFill"]');
+  var $inputCEP = document.querySelector('[data-js="inputCEP"]');
+  $formCEP.on('submit', handleSubmitCEP);
+
+  function handleSubmitCEP(event) {
+    event.preventDefault();
+    var cepSubmetido = cepOnlyNumbers($inputCEP.value);
+    openRequest(cepSubmetido);
+  }
+
+  function cepOnlyNumbers(cep){
+    var regex = /\d+/g;
+    return cep.match(regex).join('');
+  }
+
+
+  function openRequest(cepSubmetido){
+    var url = 'https://viacep.com.br/ws/'+ cepSubmetido +'/json/';
+    ajax.open('GET',url);
+    ajax.send();
+    $inputMessage.value = getMessage('loading');
+    ajax.addEventListener('readystatechange',handleReadyStateChange,false)
+  }
+
+  function handleReadyStateChange(){
+    if(isRequestOk()){
+      $inputMessage.value = getMessage('ok');
+      fillingInputs();
+     }
+  }
+
+  function isCEPValid(){
+    var result
+    try {
+        console.log('oi');
+        result = JSON.parse(ajax.responseText);
+    } catch (error) {
+
+         result =  null;
+    }
+     return result;
+  }
+
+  function fillingInputs(){
+        var responseText = isCEPValid();
+        console.log(responseText);
+        if(!responseText){
+          $inputMessage.value = getMessage('error');
+          responseText = clearData();
+        }
+        $inputsToFill.get()[0].value = responseText.logradouro;
+        $inputsToFill.get()[1].value = responseText.bairro;
+        $inputsToFill.get()[2].value = responseText.uf;
+        $inputsToFill.get()[3].value = responseText.localidade;
+        $inputsToFill.get()[4].value = responseText.cep;
+  }
+
+  function clearData(){
+     return {
+       logradouro : '',
+       bairro : '',
+       uf : '',
+       localidade : '',
+       cep : ''
+     }
+  }
+
+  function isRequestOk(){
+    return ajax.status === 200 && ajax.readyState === 4;
+  }
+
+
+  function getMessage(type){
+    return {
+        loading : 'Buscando informações para o CEP ' + cepOnlyNumbers($inputCEP.value)  + '...',
+        ok : 'Endereço referente ao CEP '+ cepOnlyNumbers($inputCEP.value)  + ' :',
+        error : 'Não encontramos o endereço para o CEP ' + cepOnlyNumbers($inputCEP.value) + '.'
+
+    }[type];
+
+
+  }
+
+
+
+
+})();
